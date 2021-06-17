6997d515e7f957202ad6fda345e0ed585a00ae6e
arrryan05 <79926995+arrryan05@users.noreply.github.com>
2021-06-17 20:37:51 +0530

    Create stacks1.cpp

---
+++ stacks&queues/stacks1.cpp
@@ -0,0 +1,135 @@
+//Stack data structure
+
+//simple implementation using  vanila array
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MyStack{
+    int *arr;
+    int cap;
+    int top;
+    MyStack(int c){
+        cap=c;
+        arr=new int [cap];
+        top=-1;
+    }
+    
+    void push(int x){
+        if(top==cap-1){cout<<"Stack is full"<<endl;return;}
+        top++;
+        arr[top]=x;
+    }
+    
+    int pop(){
+        if(top==-1){cout<<"Stack is Empty"<<endl;return INT_MIN;}
+        int res=arr[top];
+        top--;
+        return res;
+    }
+    
+    int peek(){
+        if(top==-1){cout<<"Stack is Empty"<<endl;return INT_MIN;}
+        return arr[top];
+    }
+    
+    int size(){
+        return (top+1);
+    }
+    
+    bool isEmpty(){
+        return top==-1;
+    }
+};
+
+int main()
+{
+    MyStack s(5);
+    s.push(5);
+    s.push(10);
+    s.push(20);
+    cout<<s.pop()<<endl;
+    cout<<s.size()<<endl;
+    cout<<s.peek()<<endl;
+    cout<<s.isEmpty()<<endl;
+  
+    return 0; 
+}
+
+//using vectors
+//used so that size of stack need not be predefined
+
+struct MyStack{
+    vector<int> v;
+    
+    void push(int x){
+        v.push_back(x);
+    }
+    
+    int pop(){
+        int res=v.back();
+        v.pop_back();
+        return res;
+    }
+    
+    int peek(){
+        return v.back();
+    }
+    
+    int size(){
+        return v.size();
+    }
+    
+    bool isEmpty(){
+        return v.empty();
+    }
+};
+
+//Linked list implimentation 
+//head side is used for insertion and deletion
+struct Node{           //O(1)
+    int data;
+    Node *next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+struct MyStack{
+    Node *head;
+    int sz;
+    MyStack(){
+        head=NULL;
+        sz=0;
+    }
+    
+    void push(int x){
+        Node *temp=new Node(x);
+        temp->next=head;
+        head=temp;
+        sz++;
+    }
+    
+    int pop(){
+        if(head==NULL){cout<<"Stack is Empty"<<endl;return INT_MAX;}
+        int res=head->data;
+        Node *temp=head;
+        head=head->next;
+        delete(temp);
+        sz--;
+        return res;
+    }
+    
+    int peek(){
+        if(head==NULL){cout<<"Stack is Empty"<<endl;return INT_MAX;}
+        return head->data;
+    }
+    
+    int size(){
+        return sz;
+    }
+    
+    bool isEmpty(){
+        return head == NULL;
+    }
+};
